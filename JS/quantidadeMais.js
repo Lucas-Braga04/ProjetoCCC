01df45db2424dee1711dc307c33442a18693853d
PabloR18 <pablo.oliveira11@aluno.senai.br>
2023-06-27 19:23:41 -0300

    alteração no js da quantidade de produto

--- JS/quantidadeMais.js
+++ JS/quantidadeMais.js
@@ -1,9 +1,12 @@
 function alterarQuantidade(valor) {
     var quantidadeElemento = document.getElementById('quantidade');
-
     var quantidade = parseInt(quantidadeElemento.innerHTML);
 
     quantidade += valor;
+
+    if (quantidade < 0) {
+        quantidade = 0;
+    }
 
     quantidadeElemento.innerHTML = quantidade;
 }
