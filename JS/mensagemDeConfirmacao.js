32261383f0b184b8062b8218d1444ae7a39494eb
PabloSG1 <pablo.oliveira11@aluno.senai.br>
2023-07-05 01:49:14 -0300

    correção javascript do Modal

--- JS/mensagemDeConfirmacao.js
+++ JS/mensagemDeConfirmacao.js
@@ -1,12 +1,19 @@
 function mensagemConfirmacao() {
+    var botao = event.target;
+
+    // Verifica se a mensagem já existe antes de criá-la novamente
+    var mensagemExistente = botao.nextElementSibling;
+    if (mensagemExistente && mensagemExistente.classList.contains('mensagem-temporaria')) {
+        return; // Evita que a mensagem seja criada novamente
+    }
+
     var mensagem = document.createElement("p");
     mensagem.textContent = "Produto adicionado ao carrinho!!";
-
-    var botao = document.getElementById("botao-carrinho");
+    mensagem.classList.add('mensagem-temporaria');
 
     botao.parentNode.insertBefore(mensagem, botao.nextSibling);
 
     setTimeout(function () {
         mensagem.remove();
-    }, 1000);
+    }, 2000);
 }
